deb49f5dce5bcac3d101fe5be41c86477ba2d436
WolfVdS <wolf.vandensteen@live.be>
2024-09-23 21:37:08 +0200

    Added an add_all function to the linked_list. Fixed the remove function in linked_list.

--- src/linked_list.hpp
+++ src/linked_list.hpp
@@ -9,6 +9,7 @@ template <typename T> class Linked_list
     ~Linked_list();
     void add(T* t);
     void add(int index, T* t);
+    void add_all(Linked_list<T>* list);
     T* get(int index);
     T* remove(int index);
     T* remove(T* t);
@@ -92,6 +93,36 @@ template <typename T> void Linked_list<T>::add(int index, T* t)
     }
 }
 
+template <typename T> void Linked_list<T>::add_all(Linked_list<T>* list)
+{
+    if (list == nullptr)
+    {
+        return;
+    }
+
+    if (next == nullptr)
+    {
+        if (item == nullptr)
+        {
+            item = list->item;
+            if (list->next != nullptr)
+            {
+                next = new Linked_list();
+                next->add_all(list->next);
+            }
+        }
+        else
+        {
+            next = new Linked_list();
+            next->add_all(list);
+        }
+    }
+    else
+    {
+        next->add_all(list);
+    }
+}
+
 template <typename T> T* Linked_list<T>::get(int index)
 {
     if (index == 0)
@@ -113,79 +144,104 @@ template <typename T> T* Linked_list<T>::get(int index)
 
 template <typename T> T* Linked_list<T>::remove(int index)
 {
+
     if (index < 0)
     {
         return nullptr;
     }
 
-    if (index == 0)
+    if (index == 0) // if this item needs to be removed.
     {
-        if (next != nullptr)
+        if (next != nullptr) // if there is a next list entry
         {
+            // remove the item by overriding it with the next, this shifts the list to
+            // the front
+            T* temp{item};
             item = next->item;
-            Linked_list<T>* temp{next};
-            next = next->next;
-            temp->remove(index);
-            // Shorten the list when a part is empty.
-            if (temp->next == nullptr && temp->item == nullptr)
+            if (next->next != nullptr)
             {
-                delete temp;
+                next->remove(0);
+                return temp;
             }
-            return nullptr;
+            else // if the next item is the last in the list, remove it because we
+                 // dont need it anymore.
+            {
+                delete next;
+                next = nullptr;
+                return temp;
+            }
         }
-        // We reached the end.
-        else
+        else // if this is the only item in the list (we know it is the only item
+             // in the list because it doesnt have a next one but it is also the
+             // last because it would have been deleted).
         {
+            // create an empty list entry.
+            T* temp{item};
             item = nullptr;
-            return nullptr;
-        }
-    }
-    else
-    {
-        if (index == 1)
-        {
-            T* temp = next->item;
-            next->remove(index - 1);
             return temp;
         }
-        else
+    }
+    else // if this item does not need to be removed.
+    {
+        // if the next item is the item that is going to be removed.
+        if (index - 1 == 0)
         {
-            return next->remove(index - 1);
+            // if the next item exists.
+            if (next != nullptr)
+            {
+                // save the item in temp to return later
+                T* temp = next->item;
+
+                // if the next item is the last item in the list but not the first (we
+                // know it is not the first because then index would have been 0).
+                if (next->next == nullptr)
+                {
+                    // delete the next entry to shorten the list.
+                    delete next;
+                    next = nullptr;
+                }
+                else
+                {
+                    // remove the next item.
+                    next->remove(0);
+                }
+                // return the removed item.
+                return temp;
+            }
+            else
+            {
+                return nullptr;
+            }
+        }
+        else // if the next item does not need to be removed.
+        {
+            if (next != nullptr) // if the next item exists.
+            {
+                return next->remove(index - 1);
+            }
+            else
+            {
+                return nullptr;
+            }
         }
     }
 }
 
 template <typename T> T* Linked_list<T>::remove(T* t)
 {
-    if (item == t)
+    if (item == t) // if we need to remove this item.
     {
-        if (next != nullptr)
-        {
-            item = next->item;
-            Linked_list<T>* temp{next};
-            next = next->next;
-            temp->remove(0);
-            // Shorten the list when a part is empty.
-            if (temp->next == nullptr && temp->item == nullptr)
-            {
-                delete temp;
-            }
-            return t;
-        }
-        // We reached the end.
-        else
-        {
-            item = nullptr;
-            return t;
-        }
+        // remove the item using the normal remove function.
+        remove(0);
+        return t;
     }
-    else
+    else // if this item does not need to be removed.
     {
-        if (next != nullptr)
+        if (next != nullptr) // try to remove it from the remaining list.
         {
             return next->remove(t);
         }
-        else
+        else // if the item is not in the list.
         {
             return nullptr;
         }
