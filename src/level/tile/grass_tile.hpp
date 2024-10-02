c38ae6f1401d76756eb446f2519f72ae5caecdb5
Jonas Van den Steen <jonas.vandensteen@live.be>
2024-10-02 22:12:37 +0200

    added the grass_tile 2

--- src/level/tile/grass_tile.hpp
+++ src/level/tile/grass_tile.hpp
@@ -2,11 +2,13 @@
 #define GRASS_TILE_HPP
 #include "tile.hpp"
 
-class Rock_tile : public Tile
+class Grass_tile : public Tile
 {
   public:
-    Rock_tile(uint8_t _id);
+    Grass_tile(uint8_t _id);
     void render(Level* level, int x, int y) override;
+    void tick(Level* level, int xt, int yt) override;
+    bool interact(Level* level, int xt, int yt, Player* player, Item* item, int attack_dir) override;
 };
 
 #endif
