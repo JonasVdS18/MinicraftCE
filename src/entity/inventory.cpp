3ca0439297183f764cade384b96b3fae9267bc78
WolfVdS <wolf.vandensteen@live.be>
2025-02-10 17:10:19 +0100

    Fixed problem with multiple inventory add calls for the same item.

--- src/entity/inventory.cpp
+++ src/entity/inventory.cpp
@@ -2,6 +2,7 @@
 #include "../arraylist.hpp"
 #include "../item/item.hpp"
 #include "../item/resource_item.hpp"
+#include <debug.h>
 
 Inventory::Inventory() : items{new Arraylist<Item>()}
 {
@@ -19,26 +20,50 @@ Inventory::~Inventory()
 
 void Inventory::add(Item* item)
 {
-    items->add(item);
+    if (dynamic_cast<Resource_item*>(item) != nullptr)
+    {
+        dbg_printf("dynamic cast worked\n");
+        Resource_item* to_take = static_cast<Resource_item*>(item);
+        Resource_item* has = find_resource(to_take->resource);
+        if (has == nullptr)
+        {
+            dbg_printf("hass == nullptr\n");
+            items->add(to_take);
+        }
+        else
+        {
+            dbg_printf("hass != nullptr\n");
+            has->count += to_take->count;
+        }
+    }
+    else
+    {
+        dbg_printf("dynamic cast didn't work\n");
+        items->add(item);
+    }
 }
 /*not fully implemented yet*/
 void Inventory::add(int slot, Item* item)
 {
     if (dynamic_cast<Resource_item*>(item) != nullptr)
     {
+        dbg_printf("dynamic cast worked\n");
         Resource_item* to_take = static_cast<Resource_item*>(item);
         Resource_item* has = find_resource(to_take->resource);
         if (has == nullptr)
         {
+            dbg_printf("hass == nullptr\n");
             items->add(slot, to_take);
         }
         else
         {
+            dbg_printf("hass != nullptr\n");
             has->count += to_take->count;
         }
     }
     else
     {
+        dbg_printf("dynamic cast didn't work\n");
         items->add(slot, item);
     }
 }
